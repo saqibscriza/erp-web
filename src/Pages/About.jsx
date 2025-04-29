e6733257b2295ba5752bd1ba4b9a6f6fb1e9ff8e
saqibscriza <saqib@scriza.in>
2025-04-29 10:39:16 +0530

    29/04/25

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
-import HomeThird from '../components/Home/HomeThird'
+// import HomeThird from '../components/Home/HomeThird'
 
 const About = () => {
     return (
 
         <div>
-            About 17/04/20
+            About 29/04/20
         </div>
-
-   
 
     )
 }
